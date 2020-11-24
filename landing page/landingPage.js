14617369bdecfcb58e1326f7235dd1268a87c3bd
utkrisht3108 <utkrisht2001.sharma@gmail.com>
2020-11-25 03:11:24 +0530

    read more review

--- landing page/landingPage.js
+++ landing page/landingPage.js
@@ -52,3 +52,17 @@ document.querySelector(".signup-btn").onclick = async () => {
     console.log(error);
   }
 };
+
+    var btn1 = document.querySelector(".btn1");
+    var slide = document.querySelector(".slideJ");
+    var btn2 = document.querySelector(".btn2");
+    btn1.onclick = () => {
+      btn1.classList.add("hidden");
+      slide.classList.remove("hidden");
+      btn2.classList.remove("hidden");
+    };
+    btn2.onclick = () => {
+      btn1.classList.remove("hidden");
+      slide.classList.add("hidden");
+      btn2.classList.add("hidden");
+    };
